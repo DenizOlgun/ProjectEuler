b44c45b97bf007c42e74379bd3c84a091cbf5f12
DenizOlgun <dg.olgun@gmail.com>
2018-02-25 23:52:09 -0500

    Solved Problem35.

--- src/Problems/Unfinished/Problem35.java
+++ src/Problems/Unfinished/Problem35.java
@@ -2,13 +2,33 @@ package Problems.Unfinished;
 
 import Problems.ProblemBase;
 
+import java.util.stream.IntStream;
+
 /**
  * Created by Pat111 on 2/25/2018.
  */
 public class Problem35 extends ProblemBase {
 
+    public static int rotate(int i, int executions) {
+
+        String result = String.valueOf(i);
+        result = result.substring(executions) + result.substring(0, executions);
+        return Integer.valueOf(result);
+    }
+
+    public static boolean isCircularPrime(int i) {
+
+        for(int j = 0; j < String.valueOf(i).length(); j++) {
+
+            if(!isPrime(rotate(i, j))) return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(properDivisors(5));
+        IntStream intStream = IntStream.range(2, 1000000);
+        System.out.println(intStream.filter(Problem35::isCircularPrime).count());
     }
 }
